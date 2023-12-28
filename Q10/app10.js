581604aa051a42a526c01924363840eddfae24b2
Monalisa Saha <116990954+Monalisa-24@users.noreply.github.com>
2023-12-28 11:52:01 +0530

    Add files via upload

---
+++ Q10/app10.js
@@ -0,0 +1,8 @@
+function makeTable(number){
+    for(let i=1; i<=10; i++){
+        console.log(number+"x"+i+" = "+ (number*i));
+    }
+}
+
+let number = prompt("Enter a number for the multiplication table : ");
+makeTable(number);
